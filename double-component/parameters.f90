449f28d1ec5d7649f3a2e7277f1ee3f7a4161e7e
Nex Friedrich <nex.friedrich@gmail.com>
2016-08-08 22:20:19 +0200

    removed parameter keyword

--- double-component/parameters.f90
+++ double-component/parameters.f90
@@ -8,10 +8,10 @@ integer, parameter :: i8 = selected_int_kind(18)
 complex( kind = dp ), parameter :: re = (1.0_dp, 0.0_dp)
 complex( kind = dp ), parameter :: im = (0.0_dp, 1.0_dp)
 
-integer, parameter :: stepsForJudge = 1000
-logical, parameter :: quietON = .false.
-real( kind = dp ), parameter :: convCriterion = 1.0d-08
-real( kind = dp ), parameter :: chopCutoff = 1.0d-20
-real( kind = dp ), parameter :: dt = 0.001_dp
+integer :: stepsForJudge = 1000
+logical :: quietON = .false.
+real( kind = dp ) :: convCriterion = 1.0d-08
+real( kind = dp ) :: chopCutoff = 1.0d-20
+real( kind = dp ) :: dt = 0.001_dp
 
 end module
